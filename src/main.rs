87bd503c6373b22ca8db4d85bc71b86160269918
jerome <jerome.arn@heig-vd.ch>
2021-05-07 21:36:31 +0200

    validator

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+mod login;
+mod register;
 mod validator;
 use google_authenticator::{ErrorCorrectionLevel, GoogleAuthenticator};
 use read_input::prelude::*;
-use regex::Regex;
 extern crate postgres;
 use postgres::{Connection, TlsMode};
 
 fn main() {
-
-    let conn = Connection::connect("postgresql://admin:S3c@localhost:5432/beautiful_db", TlsMode::None)
-        .unwrap();
+    let conn = Connection::connect(
+        "postgresql://admin:S3c@localhost:5432/beautiful_db",
+        TlsMode::None,
+    )
+    .unwrap();
     //let email_regex = Regex::new(r"^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,7}$").unwrap();
     //let password_regex = Regex::new(r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$").unwrap();
 
@@ -31,12 +34,7 @@ fn main() {
     loop {
         let input_token: String = input().repeat_msg("Please input your Token\n").get();
 
-        if auth.verify_code(
-            &secret,
-            &input_token,
-            0,
-            0,
-        ) {
+        if auth.verify_code(&secret, &input_token, 0, 0) {
             println!("match!");
             break;
         }
---
+++ src/validator.rs
@@ -0,0 +1,56 @@
+use lazy_static::lazy_static;
+extern crate regex;
+use regex::Regex;
+use regex::RegexSet;
+
+pub fn is_username_valid(username: &str) -> bool {
+    let mut is_valid: bool = true;
+
+    lazy_static! {
+        static ref USERNAME_REGEX: Regex = Regex::new(
+            r"^[\w_+&*-]+(?:\.[\w_+&*-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$"
+        )
+        .unwrap();
+    }
+
+    is_valid &= USERNAME_REGEX.is_match(username);
+    is_valid
+}
+
+pub fn is_password_valid(password: &str) -> bool {
+    let mut is_valid: bool = true;
+    lazy_static! {
+        static ref PASSWORD_REGEX: RegexSet =
+            RegexSet::new(&[r"[a-z]+", r"[A-Z]+", r"[0-9]+", r"[!@#$%^&*]+",r"^[\w!@#$%^&*]{8,64}$"]).unwrap();
+    }
+
+    let matches: Vec<_> = PASSWORD_REGEX.matches(password).into_iter().collect();
+    is_valid &= PASSWORD_REGEX.len() == matches.len();
+    is_valid
+}
+
+#[cfg(test)]
+mod test_globbing {
+    use super::*;
+    use rstest::rstest;
+    #[rstest(
+        input,
+        expected,
+        case("jerome.arn@heig-vd.ch", true),
+        ::trace // Traces testing for easier debugging
+    )]
+    pub fn user_valid(input: &str, expected: bool) {
+        assert_eq!(expected, is_username_valid(&input));
+    }
+
+    #[rstest(
+        input,
+        expected,
+        case("57VYJL34", false),
+        case("p^6SF%FH", true),
+        ::trace // Traces testing for easier debugging
+    )]
+    pub fn password_not_valid(input: &str, expected: bool) {
+        assert_eq!(expected, is_password_valid(&input));
+    }
+}
